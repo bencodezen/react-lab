3b586c3d8e45b294f4a5627763a70ddf32f9c596
Ben Hong <ben@bencodezen.io>
2020-09-16 07:34:07 -0400

    feature: add assignment #2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 import Assignment1 from './UdemyMax/Assignment1/Assignment1.js'
+import Assignment2 from './UdemyMax/Assignment2/Assignment2.js'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        <Assignment2 />
         <Assignment1 />
       </header>
     </div>
---
+++ src/UdemyMax/Assignment2/Assignment2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Assignment2 extends Component {
+  render() {
+    return (
+      <article>
+        <h1>Assignment 2</h1>
+      </article>
+    )
+  }
+}
+
+export default Assignment2
